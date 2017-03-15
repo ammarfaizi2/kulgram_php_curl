43cf8de06879e511df9fd9eda3ffa61f142b4f80
Ammar Faizi <ammarfaizi2@gmail.com>
2017-03-15 12:49:53 +0700

    Edit pengirim.php

--- README.md
+++ README.md
@@ -10,7 +10,3 @@ Silahkan download filenya...
 
 simpan `tujuan.php` di local anda,
 lalu ganti url pada file `pengirim.php` dengan url local anda yang mengarah ke file tujuan.php
-
-
-
-
--- pengirim.php
+++ pengirim.php
@@ -1,8 +1,20 @@
 <?php
+// instansiasi curl/memulai sesi curl
 $ch = curl_init("http://yessrilanka.com/kulgram/tujuan.php");
+
+// mengatur curl
 curl_setopt_array($ch,array(
 		CURLOPT_RETURNTRANSFER=>true
 	));	
+
+// menjalankan curl
 $out = curl_exec($ch);
+
+// tampilkan pesan error jika terjadi error
+$err = curl_errno($ch) and print $err;
+
+// menutup curl
 curl_close($ch);
+
+// tampilkan hasil curl
 print $out;
